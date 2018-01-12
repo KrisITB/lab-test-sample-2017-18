55b19924e4d4248b326191a9f61f0f619817105f
B00064110 Krzysztof Szczurowski <b00064110@student.itb.ie>
2018-01-12 11:05:09 +0000

    testingCommit

---
+++ Assets/Editor/NewEditModeTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeTest {
+
+	private StorageModel storageModel = new StorageModel(); 
+
+	[Test]
+	public void Test1() {
+		Assert.AreEqual (storageModel.GetUnits (), 0);
+	}
+}
